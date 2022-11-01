b7c9fad9f06dbfc9d9c9ffc4c2e2f6a7dad9f259
kisnak21 <krisnastya21@gmail.com>
2022-11-01 11:21:27 +0700

    menambahkan berkas tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 function App() {
-  return (
-    <div className="App">
-      <h1 style={{textAlign: 'center'}}>HELLOW GES</h1>
-    </div>
-  );
+return (
+<div className='App'>
+<h1 className='text-3xl text-teal-500 text-center'>Tes Tailwind yagesya</h1>
+<div className='flex items-center justify-center mt-10 hover:animate-bounce'>
+<div className='h-14 w-14 shadow-lg bg-sky-500'></div>
+<div className='h-14 w-14 shadow-lg bg-red-500'></div>
+<div className='h-14 w-14 shadow-lg bg-cyan-500'></div>
+</div>
+</div>
+);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import './styles/style.css';
+import './styles/App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<React.StrictMode>
+<App />
+</React.StrictMode>,
 );
