11c9b248ab7ecabe6c01643913b2a3ce83c2d008
kisnak21 <krisnastya21@gmail.com>
2022-11-01 22:16:14 +0700

    menambah konfigurasi prettier

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './styles/App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<React.StrictMode>
-<App />
-</React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
