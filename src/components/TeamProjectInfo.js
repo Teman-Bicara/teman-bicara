ad681d3cc9706853f286bdd9b3ba5a57b02797e9
alif-hanafiah <muhalifhanafiah@gmail.com>
2022-12-05 00:09:56 +0700

    memperbaiki ukuran text

--- src/components/TeamProjectInfo.js
+++ src/components/TeamProjectInfo.js
@@ -4,7 +4,7 @@ function TeamProjectInfo() {
   return (
     <section className="bg-cover bg-no-repeat bg-center pt-8 pb-16" style={{ backgroundImage: 'url(\'assets/image/background-teamproject.png\')' }}>
       <div className="max-w-[85%] mx-auto xl:max-w-[70%]">
-        <h2 className="font-bold text-base bg-white text-center py-2 mb-8 mx-auto  max-w-md shadow-2xl">Team Project</h2>
+        <h2 className="font-bold text-base bg-white text-center text-[#304652] py-2 mb-8 mx-auto  max-w-md shadow-2xl xl:text-5xl xl:py-6 xl:max-w-xl">Team Project</h2>
         <div className="grid gap-20 mt-20 md:grid-cols-2 xl:gap-32">
           <div className="bg-[#FFFFFFAD] rounded-2xl relative px-11 shadow-2xl">
             <img src="assets/image/placeholder.png" alt=" " className="rounded-full absolute top-[-3rem] left-4" />
