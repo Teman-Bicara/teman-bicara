0b27caaa035f8a9cdf1fcc7bbc9d9306ad54798a
kisnak21 <krisnastya21@gmail.com>
2022-12-04 11:14:22 +0700

    beranda forum

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import HomePageNav from '../components/HomePageNav';
+import HomePageMain from '../components/HomePageMain';
 
 export default function HomePage() {
   return (
-    <div>
-      <HomePageNav />
-    </div>
+    <>
+      <header>
+        <img src="assets/image/home.svg" alt="" className="absolute w-[375px] h-[132px] left-0 top-0" />
+        <p className="absolute w-[121px] h-[44px] left-[74px] top-11 font-semibold text-base">Selamat Datang, Laida Lavenia...</p>
+      </header>
+      <main>
+        <HomePageMain />
+      </main>
+      <footer>
+        <HomePageNav />
+      </footer>
+    </>
   );
 }
