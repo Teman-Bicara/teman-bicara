1d455d064538e84e3b9c61bf7feb9b222f5c8c49
alif-hanafiah <muhalifhanafiah@gmail.com>
2022-12-05 00:03:44 +0700

    membuat desktop version main pada landing page

--- src/components/LandingPageMain.js
+++ src/components/LandingPageMain.js
@@ -5,25 +5,25 @@ function LandingPageMain() {
   return (
     <>
       <div className="max-w-[80%] mx-auto mb-8">
-        <section className="mb-4">
-          <div className="font-montserrat">
-            <h1 className="text-2xl font-bold text-center mt-8 mb-5">
+        <section className="mb-4 md:grid md:grid-cols-2 md:gap-8 md:mb-24 md:items-center">
+          <div className="font-montserrat lg:grid">
+            <h1 className="text-2xl font-bold text-center mt-8 mb-5 md:text-right lg:text-4xl xl:text-6xl">
               Mari bercerita di
               {' '}
-              <span className="block text-[#8C4AF7]">Teman Bicara</span>
+              <span className="block text-[#8C4AF7] xl:mb-8">Teman Bicara</span>
             </h1>
-            <img src="assets/image/illustration1.svg" alt=" " className="mb-2 sm:hidden" />
-            <p className="">
+            <img src="assets/image/illustration1.svg" alt=" " className="mb-2 mx-auto md:hidden" />
+            <p className="md:text-xl">
               Teman Bicara adalah platform yang dapat memfasilitasi remaja sebagai tempat
               bercerita dalam bentuk forum yang dapat diakses melalui website Teman Bicara
               selama ada akses internet.
             </p>
           </div>
-          <img src="assets/image/illustration1.svg" alt=" " className="hidden sm:block" />
+          <img src="assets/image/illustration1.svg" alt=" " className="hidden mx-auto md:block" />
         </section>
-        <section className="mb-4">
-          <img src="assets/image/illustration2.svg" alt=" " className="mb-2" />
-          <p>
+        <section className="mb-4 md:grid md:grid-cols-2 md:gap-8 md:mb-24 md:items-center">
+          <img src="assets/image/illustration2.svg" alt=" " className="mb-2 mx-auto" />
+          <p className="md:text-xl">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
             Ipsum has been the industry standard dummy text ever since the 1500s, when an
             unknown printer took a galley of type and scrambled it to make a type specimen
@@ -32,9 +32,9 @@ function LandingPageMain() {
             the release of Letraset sheets containing...
           </p>
         </section>
-        <section>
-          <img src="assets/image/illustration3.svg" alt=" " className="mb-4" />
-          <p>
+        <section className="mb-4 md:grid md:grid-cols-2 md:gap-8 md:mb-24 md:items-center">
+          <img src="assets/image/illustration3.svg" alt=" " className="mb-4 mx-auto md:order-2" />
+          <p className="md:text-xl">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
             Ipsum has been the industry standard dummy text ever since the 1500s, when an
             unknown printer took a galley of type and scrambled it to make a type specimen
