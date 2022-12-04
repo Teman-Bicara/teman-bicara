c7ccc98d4b8ee6f9e25c41104c91dc5d9cadaaaf
alif-hanafiah <muhalifhanafiah@gmail.com>
2022-12-04 23:37:50 +0700

    slicing team project info

--- src/components/TeamProjectInfo.js
+++ src/components/TeamProjectInfo.js
@@ -2,19 +2,100 @@ import React from 'react';
 
 function TeamProjectInfo() {
   return (
-    <section className="bg-cover bg-no-repeat bg-center" style={{ backgroundImage: 'url(\'assets/image/background-teamproject.png\')' }}>
-      <div className="max-w-[80%] mx-auto pt-8">
+    <section className="bg-cover bg-no-repeat bg-center pt-8 pb-16" style={{ backgroundImage: 'url(\'assets/image/background-teamproject.png\')' }}>
+      <div className="max-w-[85%] mx-auto xl:max-w-[70%]">
         <h2 className="font-bold text-base bg-white text-center py-2 mb-8 mx-auto  max-w-md shadow-2xl">Team Project</h2>
-        <div>
-          <div className="bg-white rounded-2xl border-black border-2">
-            <img src="assets/image/placeholder.png" alt=" " className="rounded-full" />
-            <h1>Muhammad Alif Hanafiah</h1>
-            <p>Universitas Putra Indonesia Yptk Padang</p>
-            <p>
+        <div className="grid gap-20 mt-20 md:grid-cols-2 xl:gap-32">
+          <div className="bg-[#FFFFFFAD] rounded-2xl relative px-11 shadow-2xl">
+            <img src="assets/image/placeholder.png" alt=" " className="rounded-full absolute top-[-3rem] left-4" />
+            <h1 className="text-[#8C4AF7] font-bold text-2xl mt-20 mb-1">Muhammad Alif Hanafiah</h1>
+            <p className="text-[#304652] font-bold mb-4">Universitas Putra Indonesia Yptk Padang</p>
+            <p className="text-[#304652] font-bold mb-8">
               Saya seorang Front-end Development Website pemula yang berdomisili di jakarta.
               Sekarang saya adalah mahasiswa semester 7 di Universitas Nusa Mandiri
             </p>
-            <ul>
+            <ul className="flex gap-8 mb-12">
+              <li>
+                <a href="https://www.instagram.com/alifff_hanafiah/" target="_blank" rel="noreferrer">
+                  <img src="assets/icons/instagramPersonal.svg" alt="" />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/alifhanafiah/" target="_blank" rel="noreferrer">
+                  <img src="assets/icons/linkedinPersonal.svg" alt="" />
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/alifhanafiah" target="_blank" rel="noreferrer">
+                  <img src="assets/icons/githubPersonal.svg" alt="" />
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="bg-[#26235CAD] rounded-2xl relative px-11 shadow-2xl">
+            <img src="assets/image/placeholder.png" alt=" " className="rounded-full absolute top-[-3rem] left-4" />
+            <h1 className="text-[#2DF8BB] font-bold text-2xl mt-20 mb-1">Kresna Satya Nugroho</h1>
+            <p className="text-white font-bold mb-4">Universitas Putra Indonesia Yptk Padang</p>
+            <p className="text-white font-bold mb-8">
+              Saya seorang Front-end Development Website pemula yang berdomisili di jakarta.
+              Sekarang saya adalah mahasiswa semester 7 di Universitas Nusa Mandiri
+            </p>
+            <ul className="flex gap-8 mb-12">
+              <li>
+                <a href="https://www.instagram.com/alifff_hanafiah/" target="_blank" rel="noreferrer">
+                  <img src="assets/icons/instagramPersonal.svg" alt="" />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/alifhanafiah/" target="_blank" rel="noreferrer">
+                  <img src="assets/icons/linkedinPersonal.svg" alt="" />
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/alifhanafiah" target="_blank" rel="noreferrer">
+                  <img src="assets/icons/githubPersonal.svg" alt="" />
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="bg-[#304652AD] rounded-2xl relative px-11 shadow-2xl">
+            <img src="assets/image/placeholder.png" alt=" " className="rounded-full absolute top-[-3rem] left-4" />
+            <h1 className="text-[#2DF8BB] font-bold text-2xl mt-20 mb-1">Muhammad Raihan Ramadhan</h1>
+            <p className="text-white font-bold mb-4">Universitas Putra Indonesia Yptk Padang</p>
+            <p className="text-white font-bold mb-8">
+              Saya seorang Front-end Development Website pemula yang berdomisili di jakarta.
+              Sekarang saya adalah mahasiswa semester 7 di Universitas Nusa Mandiri
+            </p>
+            <ul className="flex gap-8 mb-12">
+              <li>
+                <a href="https://www.instagram.com/alifff_hanafiah/" target="_blank" rel="noreferrer">
+                  <img src="assets/icons/instagramPersonal.svg" alt="" />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/alifhanafiah/" target="_blank" rel="noreferrer">
+                  <img src="assets/icons/linkedinPersonal.svg" alt="" />
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/alifhanafiah" target="_blank" rel="noreferrer">
+                  <img src="assets/icons/githubPersonal.svg" alt="" />
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="bg-[#FFFFFFAD] rounded-2xl relative px-11 shadow-2xl">
+            <img src="assets/image/placeholder.png" alt=" " className="rounded-full absolute top-[-3rem] left-4" />
+            <h1 className="text-[#8C4AF7] font-bold text-2xl mt-20 mb-1">Laida Lavenia. H</h1>
+            <p className="text-[#304652] font-bold mb-4">Universitas Putra Indonesia Yptk Padang</p>
+            <p className="text-[#304652] font-bold mb-8">
+              Saya seorang Front-end Development Website pemula yang berdomisili di jakarta.
+              Sekarang saya adalah mahasiswa semester 7 di Universitas Nusa Mandiri
+            </p>
+            <ul className="flex gap-8 mb-12">
               <li>
                 <a href="https://www.instagram.com/alifff_hanafiah/" target="_blank" rel="noreferrer">
                   <img src="assets/icons/instagramPersonal.svg" alt="" />
