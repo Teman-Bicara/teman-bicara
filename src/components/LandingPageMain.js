e2f74b15bab3df3007fe778aac6e99791c0b39ba
kisnak21 <krisnastya21@gmail.com>
2022-12-05 10:43:15 +0700

    edit informasi team project dan informasi landing page

--- src/components/LandingPageMain.js
+++ src/components/LandingPageMain.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React from 'react';
 import TeamProjectInfo from './TeamProjectInfo';
 
@@ -13,7 +14,7 @@ function LandingPageMain() {
               <span className="block text-[#8C4AF7] xl:mb-8">Teman Bicara</span>
             </h1>
             <img src="assets/image/illustration1.svg" alt=" " className="mb-2 mx-auto md:hidden" />
-            <p className="md:text-xl">
+            <p className="md:text-xl text-justify">
               Teman Bicara adalah platform yang dapat memfasilitasi remaja sebagai tempat
               bercerita dalam bentuk forum yang dapat diakses melalui website Teman Bicara
               selama ada akses internet.
@@ -23,18 +24,17 @@ function LandingPageMain() {
         </section>
         <section className="mb-4 md:grid md:grid-cols-2 md:gap-8 md:mb-24 md:items-center">
           <img src="assets/image/illustration2.svg" alt=" " className="mb-2 mx-auto" />
-          <p className="md:text-xl">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-            Ipsum has been the industry standard dummy text ever since the 1500s, when an
-            unknown printer took a galley of type and scrambled it to make a type specimen
-            book. It has survived not only five centuries, but also the leap into electronic
-            typesetting, remaining essentially unchanged. It was popularised in the 1960s with
-            the release of Letraset sheets containing...
+          <p className="md:text-xl text-justify">
+            Kenapa harus menggunakan
+            {' '}
+            <span className="text-[#8C4AF7] font-semibold">Teman Bicara?</span>
+            {' '}
+            Karena di platform ini menyediakan forum untuk bercerita secara anonim maupun tidak bagi para remaja yang merasa ada gangguan kesehatan mental yang dialami dan bingung untuk menyampaikan apa yang sedang dialaminya.
           </p>
         </section>
         <section className="mb-4 md:grid md:grid-cols-2 md:gap-8 md:mb-24 md:items-center">
           <img src="assets/image/illustration3.svg" alt=" " className="mb-4 mx-auto md:order-2" />
-          <p className="md:text-xl">
+          <p className="md:text-xl text-justify">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
             Ipsum has been the industry standard dummy text ever since the 1500s, when an
             unknown printer took a galley of type and scrambled it to make a type specimen
--- src/components/TeamProjectInfo.js
+++ src/components/TeamProjectInfo.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React from 'react';
 
 function TeamProjectInfo() {
@@ -7,7 +8,7 @@ function TeamProjectInfo() {
         <h2 className="font-bold text-base bg-white text-center text-[#304652] py-2 mb-8 mx-auto  max-w-md shadow-2xl xl:text-5xl xl:py-6 xl:max-w-xl">Team Project</h2>
         <div className="grid gap-20 mt-20 md:grid-cols-2 xl:gap-32">
           <div className="bg-[#FFFFFFAD] rounded-2xl relative px-11 shadow-2xl">
-            <img src="assets/image/placeholder.png" alt=" " className="rounded-full absolute top-[-3rem] left-4" />
+            <img src="assets/image/profil3.jpg" alt=" " className="w-[110px] h-[110px] rounded-full bg-cover absolute top-[-3rem] left-4" />
             <h1 className="text-[#8C4AF7] font-bold text-2xl mt-20 mb-1">Muhammad Alif Hanafiah</h1>
             <p className="text-[#304652] font-bold mb-4">Universitas Putra Indonesia Yptk Padang</p>
             <p className="text-[#304652] font-bold mb-8">
@@ -34,26 +35,25 @@ function TeamProjectInfo() {
           </div>
 
           <div className="bg-[#26235CAD] rounded-2xl relative px-11 shadow-2xl">
-            <img src="assets/image/placeholder.png" alt=" " className="rounded-full absolute top-[-3rem] left-4" />
+            <img src="assets/image/profil2.jpg" alt=" " className="w-[110px] h-[110px] rounded-full bg-cover absolute top-[-3rem] left-4" />
             <h1 className="text-[#2DF8BB] font-bold text-2xl mt-20 mb-1">Kresna Satya Nugroho</h1>
-            <p className="text-white font-bold mb-4">Universitas Putra Indonesia Yptk Padang</p>
+            <p className="text-white font-bold mb-4">Universitas Tanjungpura</p>
             <p className="text-white font-bold mb-8">
-              Saya seorang Front-end Development Website pemula yang berdomisili di jakarta.
-              Sekarang saya adalah mahasiswa semester 7 di Universitas Nusa Mandiri
+              Seorang mahasiswa tingkat akhir di Universitas Tanjungpura yang sedang belajar Front-End Development. Hobi ngoding cita-cita jadi fullstuckoverflow developer.
             </p>
             <ul className="flex gap-8 mb-12">
               <li>
-                <a href="https://www.instagram.com/alifff_hanafiah/" target="_blank" rel="noreferrer">
+                <a href="https://www.instagram.com/kisnaak.ngroho/" target="_blank" rel="noreferrer">
                   <img src="assets/icons/instagramPersonal.svg" alt="" />
                 </a>
               </li>
               <li>
-                <a href="https://www.linkedin.com/in/alifhanafiah/" target="_blank" rel="noreferrer">
+                <a href="https://www.linkedin.com/in/kresna-satya-nugroho-0bb3b1227/" target="_blank" rel="noreferrer">
                   <img src="assets/icons/linkedinPersonal.svg" alt="" />
                 </a>
               </li>
               <li>
-                <a href="https://github.com/alifhanafiah" target="_blank" rel="noreferrer">
+                <a href="https://github.com/kisnak21" target="_blank" rel="noreferrer">
                   <img src="assets/icons/githubPersonal.svg" alt="" />
                 </a>
               </li>
@@ -61,9 +61,9 @@ function TeamProjectInfo() {
           </div>
 
           <div className="bg-[#304652AD] rounded-2xl relative px-11 shadow-2xl">
-            <img src="assets/image/placeholder.png" alt=" " className="rounded-full absolute top-[-3rem] left-4" />
+            <img src="assets/image/profil1.jpg" alt=" " className="w-[110px] h-[110px] rounded-full bg-cover absolute top-[-3rem] left-4" />
             <h1 className="text-[#2DF8BB] font-bold text-2xl mt-20 mb-1">Muhammad Raihan Ramadhan</h1>
-            <p className="text-white font-bold mb-4">Universitas Putra Indonesia Yptk Padang</p>
+            <p className="text-white font-bold mb-4">Universitas Nusa Mandiri</p>
             <p className="text-white font-bold mb-8">
               Saya seorang Front-end Development Website pemula yang berdomisili di jakarta.
               Sekarang saya adalah mahasiswa semester 7 di Universitas Nusa Mandiri
@@ -88,7 +88,7 @@ function TeamProjectInfo() {
           </div>
 
           <div className="bg-[#FFFFFFAD] rounded-2xl relative px-11 shadow-2xl">
-            <img src="assets/image/placeholder.png" alt=" " className="rounded-full absolute top-[-3rem] left-4" />
+            <img src="assets/image/profil3.jpg" alt=" " className="w-[110px] h-[110px] rounded-full bg-cover absolute top-[-3rem] left-4" />
             <h1 className="text-[#8C4AF7] font-bold text-2xl mt-20 mb-1">Laida Lavenia. H</h1>
             <p className="text-[#304652] font-bold mb-4">Universitas Putra Indonesia Yptk Padang</p>
             <p className="text-[#304652] font-bold mb-8">
