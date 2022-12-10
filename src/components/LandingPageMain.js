920c7d608183ab90c7e6d14906cadad0ff61d115
alif-hanafiah <muhalifhanafiah@gmail.com>
2022-12-11 03:03:27 +0700

    menambahkan aos

--- src/components/LandingPageMain.js
+++ src/components/LandingPageMain.js
@@ -1,8 +1,15 @@
-/* eslint-disable max-len */
-import React from 'react';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import React, { useEffect } from 'react';
 import TeamProjectInfo from './TeamProjectInfo';
 
 function LandingPageMain() {
+  useEffect(() => {
+    Aos.init({
+      duration: 2000,
+    });
+  }, []);
+
   return (
     <>
       <div className="max-w-[80%] mx-auto mb-8 mt-8">
@@ -22,6 +29,7 @@ function LandingPageMain() {
           </div>
           <img src="/assets/image/illustration1.svg" alt=" " className="hidden mx-auto md:block" />
         </section>
+
         <section className="mb-4 md:grid md:grid-cols-2 md:gap-8 md:mb-24 md:items-center">
           <img src="/assets/image/illustration2.svg" alt=" " className="mb-2 mx-auto" />
           <p className="md:text-xl text-justify">
@@ -32,13 +40,15 @@ function LandingPageMain() {
             Karena di platform ini menyediakan forum untuk bercerita secara anonim maupun tidak bagi para remaja yang merasa ada gangguan kesehatan mental yang dialami dan bingung untuk menyampaikan apa yang sedang dialaminya.
           </p>
         </section>
+
         <section className="mb-4 md:grid md:grid-cols-2 md:gap-8 md:mb-24 md:items-center">
           <img src="/assets/image/illustration3.svg" alt=" " className="mb-4 mx-auto md:order-2" />
           <p className="md:text-xl text-justify">
             Dengan adanya platform forum ini diharapkan para remaja lebih berani untuk menceritakan permasalahan kesehatan mental yang sedang dialami pada orang lain.
           </p>
         </section>
-        <div className="max-w-4xl p-4 text-gray-800 bg-gray-200 rounded-lg shadow mx-auto">
+
+        <div data-aos="fade-up" className="max-w-4xl p-4 text-gray-800 bg-gray-200 rounded-lg shadow mx-auto">
           <div className="mb-2">
             <div className="h-3 text-3xl text-left text-gray-600">“</div>
             <p className="px-4 text-xl text-center text-gray-600 font-inter font-semibold italic">
