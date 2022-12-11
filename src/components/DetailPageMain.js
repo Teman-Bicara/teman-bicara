ff6c52d8db547aeb8761b6a7ffc8e5246671df60
alif-hanafiah <muhalifhanafiah@gmail.com>
2022-12-11 11:46:33 +0700

    fix bug comment refresh

--- src/components/DetailPageMain.js
+++ src/components/DetailPageMain.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import parser from 'html-react-parser';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import CONFIG from '../global/config';
 import DetailPageComment from './DetailPageComment';
@@ -10,6 +10,9 @@ function DetailPageMain({ posts }) {
 
   const [commenterInput, setCommenter] = useState('');
   const [commentInput, setComment] = useState('');
+
+  const [newPosts, setNewPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const onChangeCommenterHandler = (e) => {
     setCommenter(e.target.value);
@@ -37,28 +40,45 @@ function DetailPageMain({ posts }) {
 
       setCommenter('');
       setComment('');
-
-      window.location.reload(true);
     } catch (err) {
       console.log(err);
     }
   };
+
+  useEffect(() => {
+    const fetchGetPosts = async () => {
+      try {
+        const res = await axios.get(`${CONFIG.BASE_URL}/api/posts/${id}?populate=*`);
+
+        setNewPosts(res.data.data);
+        setLoading(false);
+      } catch (error) {
+        alert(error);
+      }
+    };
+
+    fetchGetPosts();
+  }, [commenterInput]);
+
+  if (loading) {
+    return null;
+  }
 
   return (
     <div className="bg-[#E6E5F3] rounded-lg shadow-lg p-10 my-10 mx-auto max-w-2xl w-[80%]">
       <div className="flex mb-10">
         <img className="rounded-full w-10 h-10" src="/assets/image/profildefault.png" alt=" " width="100%" height="auto" />
         <div className="grow ml-4">
-          <div className="flex justify-between">
+          <div className="flex justify-between items-center">
             <h3 className="font-bold">Anonymous</h3>
-            <p className="text-gray-500">{new Date(posts.attributes.createdAt).toDateString()}</p>
+            <p className="text-gray-500 xs:text-sm">{new Date(posts.attributes.createdAt).toDateString()}</p>
           </div>
           <p>{parser(posts.attributes.desc)}</p>
         </div>
       </div>
 
       <p className="font-semibold">
-        {posts.attributes.comments.data.length}
+        {newPosts.attributes.comments.data.length}
         {' '}
         <span>Comments</span>
       </p>
@@ -91,7 +111,7 @@ function DetailPageMain({ posts }) {
           </button>
         </form>
 
-        {posts.attributes.comments.data.map((comment) => <DetailPageComment comment={comment} key={comment.id} />)}
+        {newPosts.attributes.comments.data.map((comment) => <DetailPageComment comment={comment} key={comment.id} />)}
       </div>
     </div>
   );
